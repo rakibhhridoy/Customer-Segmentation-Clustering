bfdff2db681543934916d1c0233cd328f100bcf5
rakibhhridoy <rakibhridoy63@gmail.com>
2020-08-08 17:29:43 +0600

    v1.1.0

--- script/temp.py
+++ script/temp.py
@@ -4,9 +4,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import seaborn as sns
+plt.style.use("seaborn-dark")
 # Importing the dataset
-dataset = pd.read_csv('Customers.csv')
+dataset = pd.read_csv('./data/Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
 
 # Using the dendrogram to find optimal number of clusters
